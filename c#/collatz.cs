66af5737bd9fb076d2794336af16513887579678
Jack Moshell <jack.moshell@live.mercer.edu>
2019-12-01 15:07:17 -0500

    comments

--- c#/collatz.cs
+++ c#/collatz.cs
@@ -1,12 +1,12 @@
 using System;
 using System.Collections.Generic;
-//using LongPair = System.Tuple<ulong, ulong>;
 
 class Collatz{
 	static void Main(string[] args){
 		ulong length = 0;
 		ulong tenth = 0;
 		//c# does not have typedefs, which I would like.
+		//at least it has a pair class.
 		List<Tuple<ulong, ulong> > biggest = new List<Tuple<ulong, ulong> >();
 		ulong i = 0;
 		for(i = 0; i < 10; i++){
@@ -26,8 +26,9 @@ class Collatz{
 			}
 			//largest element tracking
 			if(length >= tenth){
-				Console.WriteLine("{0} -> {1}", n, length);
+				//Console.WriteLine("{0} -> {1}", n, length);
 				biggest.RemoveAt(0);
+				//COOL THING: C# compares tuples by their first element by default.
 				biggest.Add(new Tuple<ulong, ulong>(length, n));
 				//sort
 				biggest.Sort();
@@ -36,6 +37,7 @@ class Collatz{
 		}
 		Console.WriteLine("Top 10 largest elements:\n\nBy Length:");
 		for(int j = 0; j < 10; j++){
+			//C#'s string formatting is an interesting alternative to the common prinf format.
 			Console.WriteLine("Number: {0}, \tLength: {1}",
 				biggest[j].Item2.ToString(), biggest[j].Item1.ToString());
 			//swap
