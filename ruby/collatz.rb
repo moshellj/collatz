89647ea88999ab3ec9b38a39c10c600469983365
Jack Moshell <jack.moshell@live.mercer.edu>
2019-12-03 12:00:46 -0500

    largest element tracking

--- ruby/collatz.rb
+++ ruby/collatz.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/ruby -w
 
+$biggest = Array.new(10) { Array.new(2, 0) }
+$tenth = 0
 $maxlength = 0
 for n in 2..1000000
 	length = 0
@@ -12,10 +14,21 @@ for n in 2..1000000
 		end
 		length += 1
 	end
-	if length > $maxlength then
-		$maxlength = length
-		print n
-		print " -> "
-		puts length
+	if length >= $tenth then
+		$biggest[0] = [n, length]
+		$biggest = $biggest.sort_by(&:last)
+		#puts "Number = %d, Length = %d"%[n, length]
+		$tenth = $biggest[0][1]
 	end
+end
+
+puts "Largest elements:\n\nBy Length:"
+
+for x in $biggest do
+	puts "Number = %d, Length = %d"%[x[0], x[1]]
+end
+$biggest = $biggest.sort_by(&:first)
+puts "\nBy Size:"
+for x in $biggest do
+	puts "Number = %d, Length = %d"%[x[0], x[1]]
 end
