131fc0af2e024fb7a04020f9b7b7ffc17c5ef8db
Jack Moshell <jack.moshell@live.mercer.edu>
2019-12-05 11:54:13 -0500

    cpp recursion version

---
+++ cpp/recursed/recollatz.cpp
@@ -0,0 +1,65 @@
+#include <cstdio>
+#include <utility>
+#include <stdint.h>
+#include <deque>
+#include <algorithm>
+
+//A couple of testing limits.
+#define FIVE_BIL 5000000001
+#define FIVE_MIL 5000001
+#define TWTY_MIL 20000001
+#define ONE_MIL 1000001
+
+//Typedef! renames a type. shortens big ol' templated type name into something more convenient.
+typedef std::pair<uint64_t, uint64_t> intpair;
+
+uint64_t getLength(uint64_t i){
+	if(i == 1){
+		return 0;
+	}else if(i & 1){
+		return getLength(i+i+i+1) + 1;
+	}else{
+		return getLength(i >> 1) + 1;
+	}
+}
+
+int main(){
+	uint64_t i = 1;
+	uint64_t length = 0;
+	unsigned int odd = 0;
+	//initialize queue of longest sequences.
+	//pairs are length - n
+	std::deque<intpair> biggest;
+	for(int i = 0; i < 10; i++){
+		biggest.push_back(intpair(0, 0));
+	}
+	uint64_t tenth = 0;
+	
+	for(uint64_t n = 1; n < ONE_MIL; ++n){
+		length = getLength(n);
+		if(length > tenth){
+			//new element for list
+			//printf("n= %lu, L= %lu\n", n, length);//info; comment out later
+			biggest.pop_front();
+			biggest.push_back(intpair(length, n));
+			std::sort(biggest.begin(), biggest.end());
+			//new lowest member
+			tenth = biggest.front().first;
+		}
+	}
+	//print out contents
+	printf("Ten highest elements:\n\n");
+	printf("By length:\n");
+	for(std::deque<intpair>::iterator j = biggest.begin(); j != biggest.end(); j++){
+		printf("Number = %lu, Length = %lu\n", j -> second, j-> first);
+		//reorder
+		uint64_t temp = j -> first;
+		j -> first = j -> second;
+		j -> second = temp;
+	}
+	std::sort(biggest.begin(), biggest.end());//re-sort by N
+	printf("\nBy size:\n");
+	for(std::deque<intpair>::iterator j = biggest.begin(); j != biggest.end(); j++){
+		printf("Number = %lu, Length = %lu\n", j -> first, j -> second);
+	}
+}
