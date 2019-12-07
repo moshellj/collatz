76b75ca25e799ca7afa25497f7c76a9519b53b55
Jack Moshell <jack.moshell@live.mercer.edu>
2019-12-06 21:35:19 -0500

    lisp recursion

--- lisp/recursed/recollatz.lisp
+++ lisp/recursed/recollatz.lisp
@@ -3,7 +3,7 @@
 ; Let it never be said that lisp has intuitive array/list access operations. ;
 
 (defvar temp 0)
-;;bubble sort, largest to smallest.
+;;bubble sort, largest to smallest. couldn't figure out the standard sort.
 (defun bubblesort (seq)
 	(loop for a in '(8 7 6 5 4 3 2 1 0) do
 		(loop for b from 0 to a do
@@ -16,23 +16,22 @@
 	)
 )
 
+;;; Lisp is just utterly bizare. this is an if-elseif-else statement. Could you tell at a glance? (no)
+(defun getlength (i)
+	(cond 
+		((<= i 1) 0)
+		( (oddp i) (+ 1 (getlength (+ 1 (* i 3)))) )
+		(t (+ 1 (getlength (/ i 2))) )
+	)
+)
+
 ; LISP features arbitrary-size integers by default.
-(defvar i 1)
 (defvar len 0)
 (defvar minlen 0)
 (defvar biggest (make-list 10 :initial-element '(0 0)))
 ;calculate
-(dotimes (n 10000000)
-	(setq i n)
-	(setq len 0)
-	(loop
-		(if (oddp i)
-			(setq i (+ 1 (* i 3)))
-			(setq i (/ i 2))
-		)
-		(incf len)
-		(unless (> i 1) (return))
-	)
+(dotimes (n 1000000)
+	(setq len (getlength n))
 	;tracking
 	(when (>= len minlen)
 		(setf (elt biggest 9) (list len n))
